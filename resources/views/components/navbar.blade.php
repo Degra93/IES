fdf9116037d24b69f6896ae09c5e77e87d91f769
Marco De Grande <marco.degrande@gmail.com>
2023-06-07 21:06:26 +0200

    inizio navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg bg-custom navbar-expand-lg bg-body-tertiary ">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img class="img-fluid img-logo" src="{{ URL::asset('img/logo.jpg') }}" alt="Logo" width="100" height="50" class="d-inline-block align-text-top">
+          </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav d-flex justify-content-end">
+          <li class="nav-item">
+            <a class="nav-link btn btn-primary border border-radius-" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn-primary border border-radius" href="#">Chi siamo</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn-primary border border-radius" href="#">I nostri lavori</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn-primary border border-radius">Servizi</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn-primary border border-radius">Contatti</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
